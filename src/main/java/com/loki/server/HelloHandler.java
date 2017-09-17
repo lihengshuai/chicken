a97f4184232990e56f80c11f885612e149437d76
lihengshuai <1334771961@qq.com>
2017-09-17 20:32:22 +0800

    add netty server demo

---
+++ src/main/java/com/loki/server/HelloHandler.java
@@ -0,0 +1,48 @@
+package com.loki.server;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author   loki on 2017/9/17.
+ */
+public class HelloHandler extends SimpleChannelHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloHandler.class);
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        logger.info("message  received !");
+        logger.info("get message info :"+e.getMessage());
+        //send message
+        ctx.getChannel().write("hi");
+        super.messageReceived(ctx, e);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        logger.warn("there  is a  exception happen !");
+        super.exceptionCaught(ctx, e);
+    }
+
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        logger.info("channel connected !");
+        super.channelConnected(ctx, e);
+    }
+
+    @Override
+    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        super.channelDisconnected(ctx, e);
+    }
+
+    @Override
+    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        super.channelClosed(ctx, e);
+    }
+}
--- src/main/java/com/loki/server/NettyServer.java
+++ src/main/java/com/loki/server/NettyServer.java
@@ -1,10 +1,40 @@
 package com.loki.server;
 
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author loki on 2017/9/17.
  */
 public class NettyServer {
 
     public static void main(String[] args) {
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        ExecutorService boss = Executors.newCachedThreadPool();
+        ExecutorService worker = Executors.newCachedThreadPool();
+
+        bootstrap.setFactory(new NioServerSocketChannelFactory(boss,worker));
+
+        bootstrap.setPipelineFactory(() -> {
+            ChannelPipeline pipeline = Channels.pipeline();
+            pipeline.addLast("encoder",new StringEncoder());
+            pipeline.addLast("decoder",new StringDecoder());
+            pipeline.addLast("Handler", new HelloHandler());
+            return pipeline;
+        });
+        bootstrap.bind(new InetSocketAddress(8000));
+        System.out.println("server open !");
     }
 }
