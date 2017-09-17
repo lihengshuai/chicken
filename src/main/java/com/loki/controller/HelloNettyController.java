95e70458a41596a493b4f3c410754dd7e66e1a41
lihengshuai <1334771961@qq.com>
2017-09-17 11:06:56 +0800

    the  first code : say hello

---
+++ src/main/java/com/loki/controller/HelloNettyController.java
@@ -0,0 +1,21 @@
+package com.loki.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by loki on 2017/9/17.
+ */
+@RestController
+public class HelloNettyController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloNettyController
+    .class);
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return new String("Hello netty !");
+    }
+}
